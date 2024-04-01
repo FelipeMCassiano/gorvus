3d9e0961f08650c0eb97619dae956cb12518b6d5
FelipeMCassiano <felipemcassiano@gmail.com>
2024-04-01 11:53:40 -0300

    fix: templates import

--- gorvus/cmd/createDockerfile.go
+++ gorvus/cmd/createDockerfile.go
@@ -4,12 +4,12 @@ import (
 	"fmt"
 	"log"
 
-	templates "github.com/FelipeMCassiano/gorvus/gorvus/template"
+	"github.com/FelipeMCassiano/gorvus/gorvus/templates"
 	"github.com/spf13/cobra"
 )
 
 var createDockerfileCmd = &cobra.Command{
-	Use:   "create Dockerfile [language]",
+	Use:   "createDockerfile [language]",
 	Short: "Create Dockerfile based on input language and project name",
 	Run: func(cmd *cobra.Command, args []string) {
 		projectName := args[0]
