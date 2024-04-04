ecd79ce4b8b71e4e356da71b05fdbe9ad327eb5d
FelipeMCassiano <felipemcassiano@gmail.com>
2024-04-04 13:33:58 -0300

    feat: create more detailed error message

--- internal/utils/verifyLang.go
+++ internal/utils/verifyLang.go
@@ -12,6 +12,12 @@ func VerifyIfLangIsSupported(language string) {
 
 	if !ok {
 		fmt.Printf("The language '%s' is not supported\n", language)
+
+		similarLangs := FindSimilarLangs(language)
+		if similarLangs != "" {
+			fmt.Printf("Did you mean %v?\n", similarLangs)
+		}
+
 		os.Exit(1)
 	}
 }
