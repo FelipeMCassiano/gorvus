b9139686b26c9f2cfb081c800a0af5548978f822
FelipeMCassiano <felipemcassiano@gmail.com>
2024-05-02 15:19:56 -0300

    feat: create kubenertest new command

---
+++ internal/commands/kube.go
@@ -0,0 +1,25 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/FelipeMCassiano/gorvus/internal/commands/subcommands"
+	"github.com/jedib0t/go-pretty/v6/text"
+	"github.com/spf13/cobra"
+)
+
+func CreateKubeCommand() *cobra.Command {
+	kubecmd := &cobra.Command{
+		Use:   "kube",
+		Short: "Manages yaml kubernetes files",
+		Run: func(cmd *cobra.Command, args []string) {
+			if err := cmd.Help(); err != nil {
+				fmt.Println(text.FgRed.Sprint(err))
+				os.Exit(1)
+			}
+		},
+	}
+	kubecmd.AddCommand(subcommands.CreateKubeNewCommand())
+	return kubecmd
+}
